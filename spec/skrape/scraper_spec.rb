afb4df2f8a8b2141ab7e300207969c115b728d91
Draiken <luiz.felipe.gp@gmail.com>
2013-02-05 22:32:49 -0200

    Separating scraper specs

---
+++ spec/skrape/scraper_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+require 'nokogiri'
+describe Skrape::Scraper do
+
+  describe "#sel" do
+    it "should add a selector with a mapping" do
+
+      Skrape::Selector.should_receive(:new).twice
+
+      scraper = Skrape.define do
+        sel "h1", :title
+        sel "ul.items > li", :items do
+          sel "span.title", :title
+        end
+      end
+
+      scraper.selectors.size.should == 2
+    end
+  end
+
+  describe "#scrape" do
+    it "should scrape the html with its defined selector/maps" do
+      scraper = Skrape.define do
+        sel "h1", :title
+        sel "ul.items > li", :items do
+          sel "span.title", :title
+          sel ".thing", :thing do
+            sel ".one", :one
+            sel ".two", :two
+          end
+        end
+      end
+
+      html = "<html>
+          <body>
+            <h1>My title</h1>
+            <ul class=\"items\">
+              <li>
+                <span class=\"title\">First title</span>
+                <div class=\"thing\">
+                  <span class=\"one\">One</span>
+                  <span class=\"two\">Two</span>
+                </div>
+              </li>
+              <li>
+                <span class=\"title\">Last title</span>
+                <div class=\"thing\">
+                  <span class=\"one\">One</span>
+                  <span class=\"two\">Two</span>
+                </div>
+              </li>
+            </ul>
+          </body>
+        </html>"
+        parsed_html = ::Nokogiri::HTML(html)
+
+        object = scraper.scrape(parsed_html)
+
+        object[:title].should == "My title"
+        object[:items].should be_a Array
+        object[:items].count.should == 2
+        item = object[:items].last
+        item[:title].should == "Last title"
+        item[:thing][:one].should == "One"
+    end
+  end
+end
--- spec/skrape_spec.rb
+++ spec/skrape_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe Skrape do
 
-
   describe "#define" do
     it "should create a new scraper with the defined block" do
 
@@ -17,71 +16,4 @@ describe Skrape do
     end
   end
 
-  require 'ostruct'
-  require 'nokogiri'
-  describe Skrape::Scraper do
-
-    describe "#sel" do
-      it "should add a selector with a mapping" do
-
-        Skrape::Selector.should_receive(:new).twice
-
-        scraper = Skrape.define do
-          sel "h1", :title
-          sel "ul.items > li", :items do
-            sel "span.title", :title
-          end
-        end
-
-        scraper.selectors.size.should == 2
-      end
-    end
-
-    describe "#scrape" do
-      it "should scrape the html with its defined selector/maps" do
-        scraper = Skrape.define do
-          sel "h1", :title
-          sel "ul.items > li", :items do
-            sel "span.title", :title
-            sel ".thing", :thing do
-              sel ".one", :one
-              sel ".two", :two
-            end
-          end
-        end
-
-        html = "<html>
-          <body>
-            <h1>My title</h1>
-            <ul class=\"items\">
-              <li>
-                <span class=\"title\">First title</span>
-                <div class=\"thing\">
-                  <span class=\"one\">One</span>
-                  <span class=\"two\">Two</span>
-                </div>
-              </li>
-              <li>
-                <span class=\"title\">Last title</span>
-                <div class=\"thing\">
-                  <span class=\"one\">One</span>
-                  <span class=\"two\">Two</span>
-                </div>
-              </li>
-            </ul>
-          </body>
-        </html>"
-        parsed_html = ::Nokogiri::HTML(html)
-
-        object = scraper.scrape(parsed_html)
-
-        object[:title].should == "My title"
-        object[:items].should be_a Array
-        object[:items].count.should == 2
-        item = object[:items].last
-        item[:title].should == "Last title"
-        item[:thing][:one].should == "One"
-      end
-    end
-  end
 end
